c31afbbbf4ed03755972bddfd0dd510fe4527a4c
uwriym <rim2wo@gmail.com>
2024-03-28 21:49:15 +0900

    solve: 몫 구하기

---
+++ lv0/몫 구하기.py
@@ -0,0 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+def solution(num1, num2):
+    return num1//num2
+
+
+# main function
+def main():
+    result = solution(10, 5)
+    print(result)
+
+
+# run program
+if __name__ == "__main__":
+    main()
