4080dd4cae1af3cd21d59fbe5b346b0ab39fe528
uwriym <rim2wo@gmail.com>
2024-03-30 13:57:56 +0900

    solve: 두 수의 차

---
+++ lv0/두 수의 차.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+solution = lambda num1, num2: num1 - num2
+
+
+# main function
+def main():
+    result = solution(2, 3)
+    print(result)
+
+
+# run program
+if __name__ == "__main__":
+    main()
