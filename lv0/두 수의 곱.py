e0f1b9c83ac488afef08dd32236bb3780e97d780
uwriym <rim2wo@gmail.com>
2024-03-30 13:56:14 +0900

    solve: 두 수의 곱

---
+++ lv0/두 수의 곱.py
@@ -0,0 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+def solution(num1, num2):
+    return num1 * num2
+
+
+# main function
+def main():
+    result = solution(3, 4)
+    print(result)
+
+
+# run program
+if __name__ == "__main__":
+    main()
