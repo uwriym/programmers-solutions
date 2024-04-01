213c68511431e08a63529563734da887cdbd4259
uwriym <rim2wo@gmail.com>
2024-04-01 23:28:12 +0900

    solve: 나머지 구하기

---
+++ lv0/나머지 구하기.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120810
+
+solution = lambda num1, num2: num1 % num2
+
+# main function
+def main():
+    result = solution(3, 2)
+    print(result)
+
+
+# run program
+if __name__ == "__main__":
+    main()
