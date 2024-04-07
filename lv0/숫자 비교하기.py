af60b7e69fccced35796f31d137d1f04e62b0e8c
uwriym <rim2wo@gmail.com>
2024-04-07 22:09:56 +0900

    solve: 숫자 비교하기

---
+++ lv0/숫자 비교하기.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+solution = lambda num1, num2: 1 if num1 == num2 else -1
+
+# main function
+def main():
+    result = solution(2, 3)
+    print(result)
+
+
+# run program
+if __name__ == "__main__":
+    main()
