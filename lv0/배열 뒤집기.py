664d271ffc72c82d300c9a21b3fc7bc28a33e1b0
uwriym <rim2wo@gmail.com>
2024-04-27 23:04:33 +0900

    solve: 배열 뒤집기

---
+++ lv0/배열 뒤집기.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+def solution(num_list):
+    return num_list[::-1]
+
+# main function
+def main():
+    result = solution([1, 2, 3, 4, 5])
+    print(result)
+
+
+# run program
+if __name__ == "__main__":
+    main()
