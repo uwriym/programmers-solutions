cebfc0a2b0f55ef4228be5be9cf49552a452279c
uwriym <rim2wo@gmail.com>
2024-05-02 15:26:24 +0900

    solve: 나이 출력

---
+++ lv0/나이 출력.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120820?language=python3
+
+solution = lambda age: (2022 - age + 1)
+
+
+# main 함수
+def main():
+    print(solution(40))
+
+
+# 프로그램 실행
+if __name__ == "__main__":
+    main()
