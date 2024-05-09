628c11f87e03301444fdeef74b9ab9ff29e87e04
uwriym <rim2wo@gmail.com>
2024-05-09 10:59:26 +0900

    solve: 짝수는 싫어요

---
+++ lv0/짝수는 싫어요.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120817
+
+def solution(n):
+    num_list = list(x for x in range(0, n + 1))
+    even_num_list = list(x * 2 for x in range(0, n // 2 + 1))
+    return list(set(num_list) - set(even_num_list))
+
+
+# main function
+def main():
+    result = solution(10)
+    print(result)
+
+# [1,2,3]
+# run program
+if __name__ == "__main__":
+    main()
